f3b3cf742af4a709d9805bfc3d7b697672622b23
huan95 <leduyhuan95@gmail.com>
2019-01-03 00:55:29 +0700

    shell script example

---
+++ huan.sh
@@ -0,0 +1,15 @@
+echo " Torres "; mkdir 1-1-2019; cd 1-1-2019; wget https://kenh14cdn.com/2017/images685542-a-1489655177057.jpg; cd -;
+echo " Gerrard "; mkdir 2-1-2019; cd 2-1-2019; wget https://znews-photo.zadn.vn/w660/Uploaded/mdf_Nexqxk/2016_11_24/GerrardUCL.jpg; cd -;
+echo " Alison "; mkdir 3-1-2019; cd 3-1-2019; wget https://images.vov.vn/w490/uploaded/xmwqrjdykqly8uzveukg/2018_07_20/alisson_chinh_thuc_gia_nhap_liverpool_ooqu.jpg; cd -;
+echo "Van Dijk"; mkdir 4-1-2019; cd 4-1-2019; wget https://znews-photo.zadn.vn/w1024/Uploaded/ofh_huqfztmf/2018_12_29/van_dijk.jpg; cd -;
+echo " Mohamed Salah "; mkdir 5-1-2019; cd 5-1-2019; wget https://znews-photo.zadn.vn/w660/Uploaded/jopluat/2018_03_21/Salah_1.jpg; cd -;
+echo " Roberto Firmino "; mkdir 6-1-2019; cd 6-1-2019; wget https://static.bongda24h.vn/medias/standard/2017/12/27/Liverpool-gia-han-voi-tien-dao-Roberto-Firmino-hinh-anh.jpg; cd -;
+echo " Sadio Mane "; mkdir 7-1-2019; cd 7-1-2019; wget http://media.bongda.com.vn/files/phuc.tran/2016/11/08/mane-liverpool-1710.jpg; cd -;
+
+
+
+
+
+
+
+
